4ac7a9868fe7f135d418d4804b1746ff43c3033a
anjalirajput123 <31334871+anjalirajput123@users.noreply.github.com>
2017-09-10 00:19:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,9 @@ console.log('Loaded!');
 // new things
 var element=document.getElementById("main text");
 element.innerHTML='new value';
+
+//move the picture
+var img=document.getElementById('img');
+img.onclick = function() {
+    img.style.marginleft='100px';
+};
