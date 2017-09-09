1accc29b9fcaa81b84d3417d93815f59363232d9
anjalirajput123 <31334871+anjalirajput123@users.noreply.github.com>
2017-09-10 00:26:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,5 +6,6 @@ element.innerHTML='new value';
 //move the picture
 var img=document.getElementById('my buddy');
 img.onclick = function() {
+    var interval = setInterval(moveleft,100);
     img.style.marginleft='100px';
 };
